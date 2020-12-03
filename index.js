b63c78968603d0c2e16c6244d94f831fce4752a9
Arthur Lima <art.lima16@hotmail.com>
2020-12-02 21:22:39 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const Koa = require('koa');
+const bodyparser = require('koa-bodyparser');
+const router = require('./src/routes');
+
+const server = new Koa();
+
+server.use(bodyparser());
+server.use(router.routes());
+
+server.listen(8081, () => 
+console.log('Funcionando...'));
